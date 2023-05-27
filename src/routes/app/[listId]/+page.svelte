9b32249fc95d74537f5a6da5220ee422341d9a5d
nichwch <nichwch@gmail.com>
2023-05-26 22:39:25 -0400

    drag and drop...

--- src/routes/app/[listId]/+page.svelte
+++ src/routes/app/[listId]/+page.svelte
@@ -1,6 +1,30 @@
 <script>
+	import { flip } from 'svelte/animate';
+	import { dndzone } from 'svelte-dnd-action';
+	let items = [
+		{ id: 1, name: 'item1' },
+		{ id: 2, name: 'item2' },
+		{ id: 3, name: 'item3' },
+		{ id: 4, name: 'item4' }
+	];
+	const flipDurationMs = 300;
+	function handleDndConsider(e) {
+		items = e.detail.items;
+	}
+	function handleDndFinalize(e) {
+		items = e.detail.items;
+	}
 </script>
 
 <div class="p-4">
-	<h1>the app!</h1>
+	<div>TODOs</div>
+	<section
+		use:dndzone={{ items, flipDurationMs }}
+		on:consider={handleDndConsider}
+		on:finalize={handleDndFinalize}
+	>
+		{#each items as item (item.id)}
+			<div animate:flip={{ duration: flipDurationMs }}>{item.name}</div>
+		{/each}
+	</section>
 </div>
