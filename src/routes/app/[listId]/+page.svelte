fbac7603f44699a6ef1a336ca58c06ddf04a3c2a
nichwch <nichwch@gmail.com>
2023-05-29 23:55:33 -0400

    margins

--- src/routes/app/[listId]/+page.svelte
+++ src/routes/app/[listId]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { flip } from 'svelte/animate';
 	import { dndzone } from 'svelte-dnd-action';
+	import { nanoid } from 'nanoid';
 	let items = [
 		{ id: 1, name: 'item1' },
 		{ id: 2, name: 'item2' },
@@ -14,17 +15,33 @@
 	function handleDndFinalize(e) {
 		items = e.detail.items;
 	}
+	$: {
+		console.log(items);
+	}
 </script>
 
-<div class="p-4">
+<div class="p-10 max-w-2xl mx-auto">
 	<div>TODOs</div>
+	<button
+		on:click={() => {
+			items = [
+				...items,
+				{
+					id: nanoid(),
+					name: 'new todo'
+				}
+			];
+		}}>add todo</button
+	>
 	<section
 		use:dndzone={{ items, flipDurationMs }}
 		on:consider={handleDndConsider}
 		on:finalize={handleDndFinalize}
 	>
 		{#each items as item (item.id)}
-			<div animate:flip={{ duration: flipDurationMs }}>{item.name}</div>
+			<div animate:flip={{ duration: flipDurationMs }}>
+				<input placeholder="untitled task" bind:value={item.name} />
+			</div>
 		{/each}
 	</section>
 </div>
