9aec4e022a6f5babe3f00501ab723f4e7f2d3df1
nichwch <nichwch@gmail.com>
2023-06-06 00:03:16 -0400

    reeeefactor

---
+++ src/components/Todo.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let item: any;
+	export let focusedElement: string | undefined = undefined;
+</script>
+
+<div
+	id={item.id}
+	class:border={focusedElement === item.id}
+	class:border-gray-500={focusedElement === item.id}
+	class:border-b={focusedElement !== item.id}
+	class:border-b-gray-500={focusedElement !== item.id}
+	class="flex"
+>
+	<div class="w-10 h-10 bg-slate-200 inline-block border-r border-r-gray-500" />
+	<textarea
+		class="h-10 resize-none whitespace-nowrap flex-grow px-4 py-2"
+		placeholder="untitled task"
+		bind:value={item.name}
+	/>
+	<a href={`${$page.url}/${item.id}`}>expand</a>
+</div>
--- src/routes/app/[listId]/[...tasks]/+page.svelte
+++ src/routes/app/[listId]/[...tasks]/+page.svelte
@@ -4,6 +4,7 @@
 	import { nanoid } from 'nanoid';
 	import { fade, fly } from 'svelte/transition';
 	import { page } from '$app/stores';
+	import Todo from '../../../../components/Todo.svelte';
 
 	let items: any[] = [];
 	const flipDurationMs = 300;
@@ -55,26 +56,11 @@
 			use:dndzone={{ items, flipDurationMs, transformDraggedElement }}
 			on:consider={handleDndConsider}
 			on:finalize={handleDndFinalize}
-			class="overflow-y:scroll"
+			class="overflow-y-auto flex-grow"
 		>
 			{#each items as item (item.id)}
-				<div
-					id={item.id}
-					in:fly
-					animate:flip={{ duration: flipDurationMs }}
-					class:border={focusedElement === item.id}
-					class:border-gray-500={focusedElement === item.id}
-					class:border-b={focusedElement !== item.id}
-					class:border-b-gray-500={focusedElement !== item.id}
-					class="flex"
-				>
-					<div class="w-10 h-10 bg-slate-200 inline-block border-r border-r-gray-500" />
-					<textarea
-						class="h-10 resize-none whitespace-nowrap flex-grow px-4 py-2"
-						placeholder="untitled task"
-						bind:value={item.name}
-					/>
-					<a href={`${$page.url}/${item.id}`}>expand</a>
+				<div animate:flip={{ duration: flipDurationMs }} in:fly>
+					<Todo {item} />
 				</div>
 			{/each}
 		</section>
