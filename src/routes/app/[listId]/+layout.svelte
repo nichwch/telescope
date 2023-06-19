fa7c5861947114b3fc9fd595d2e6066d0dd29ae9
nichwch <nichwch@gmail.com>
2023-06-19 10:28:06 -0400

    extract data loading to layout
    attempt to update strategic goal input

--- src/routes/app/[listId]/+layout.svelte
+++ src/routes/app/[listId]/+layout.svelte
@@ -1,23 +1,35 @@
 <script lang="ts">
-	import { flip } from 'svelte/animate';
-	import { dndzone } from 'svelte-dnd-action';
-	import { nanoid } from 'nanoid';
 	import { fade } from 'svelte/transition';
 	import { page } from '$app/stores';
-	let items = [
-		{ id: 1, name: 'item1' },
-		{ id: 2, name: 'item2' },
-		{ id: 3, name: 'item3' },
-		{ id: 4, name: 'item4' }
-	];
-	const flipDurationMs = 300;
-	function handleDndConsider(e: any) {
-		items = e.detail.items;
-	}
-	72;
-	function handleDndFinalize(e: any) {
-		items = e.detail.items;
-	}
+	import { onDestroy } from 'svelte';
+
+	export let data;
+	const { supabase } = data;
+	$: strategic_goal_input = data.listContent?.[0].strategic_goal || '';
+	// $: strategic_goal_input = data.listContent?.[0].strategic_goal || '';
+	let last_strategic_goal_input = strategic_goal_input;
+	let isFlushing = false;
+	const updateInterval = setInterval(async () => {
+		if (strategic_goal_input === last_strategic_goal_input) return;
+		if (isFlushing) return;
+		isFlushing = true;
+		console.log('flushing', strategic_goal_input);
+		supabase
+			.from('lists')
+			.update({
+				strategic_goal: strategic_goal_input
+			})
+			.eq('id', $page.params.listId)
+			.then((res) => {
+				console.log('flushed!');
+				isFlushing = false;
+				last_strategic_goal_input = strategic_goal_input;
+			});
+	}, 300);
+
+	onDestroy(() => {
+		clearInterval(updateInterval);
+	});
 </script>
 
 {#key $page.params.listId}
@@ -26,6 +38,7 @@
 			<div class="mb-2 border border-gray-500 bg-green-100 flex-grow w-80 flex flex-col">
 				<div class="px-4 py-1 border-b border-b-gray-500">strategic goal</div>
 				<textarea
+					bind:value={strategic_goal_input}
 					class="p-4 bg-green-100 flex-grow w-full block resize-none focus:outline-none focus:bg-green-200 transition-all"
 					placeholder="describe your large-level goals for this project..."
 				/>
