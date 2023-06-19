f35d7c5e08c845a08d60053696e5dc101ad74187
nichwch <nichwch@gmail.com>
2023-06-18 20:58:43 -0400

    formatter

--- src/DatabaseDefinitions.ts
+++ src/DatabaseDefinitions.ts
@@ -1,93 +1,87 @@
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json }
-  | Json[]
+export type Json = string | number | boolean | null | { [key: string]: Json } | Json[];
 
 export interface Database {
-  public: {
-    Tables: {
-      lists: {
-        Row: {
-          created_date: string | null
-          id: string
-          last_edited_date: string | null
-          name: string | null
-          owner: string
-          strategic_goal: string | null
-          tasks_blob: Json | null
-        }
-        Insert: {
-          created_date?: string | null
-          id?: string
-          last_edited_date?: string | null
-          name?: string | null
-          owner: string
-          strategic_goal?: string | null
-          tasks_blob?: Json | null
-        }
-        Update: {
-          created_date?: string | null
-          id?: string
-          last_edited_date?: string | null
-          name?: string | null
-          owner?: string
-          strategic_goal?: string | null
-          tasks_blob?: Json | null
-        }
-        Relationships: []
-      }
-      tasks: {
-        Row: {
-          content: string | null
-          creation_date: string
-          finished: boolean
-          id: string
-          last_edited_date: string
-          parent_list: string | null
-          parent_task: string | null
-        }
-        Insert: {
-          content?: string | null
-          creation_date?: string
-          finished?: boolean
-          id?: string
-          last_edited_date?: string
-          parent_list?: string | null
-          parent_task?: string | null
-        }
-        Update: {
-          content?: string | null
-          creation_date?: string
-          finished?: boolean
-          id?: string
-          last_edited_date?: string
-          parent_list?: string | null
-          parent_task?: string | null
-        }
-        Relationships: [
-          {
-            foreignKeyName: "tasks_parent_list_fkey"
-            columns: ["parent_list"]
-            referencedRelation: "lists"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
-    }
-    Views: {
-      [_ in never]: never
-    }
-    Functions: {
-      [_ in never]: never
-    }
-    Enums: {
-      [_ in never]: never
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+	public: {
+		Tables: {
+			lists: {
+				Row: {
+					created_date: string | null;
+					id: string;
+					last_edited_date: string | null;
+					name: string | null;
+					owner: string;
+					strategic_goal: string | null;
+					tasks_blob: Json | null;
+				};
+				Insert: {
+					created_date?: string | null;
+					id?: string;
+					last_edited_date?: string | null;
+					name?: string | null;
+					owner: string;
+					strategic_goal?: string | null;
+					tasks_blob?: Json | null;
+				};
+				Update: {
+					created_date?: string | null;
+					id?: string;
+					last_edited_date?: string | null;
+					name?: string | null;
+					owner?: string;
+					strategic_goal?: string | null;
+					tasks_blob?: Json | null;
+				};
+				Relationships: [];
+			};
+			tasks: {
+				Row: {
+					content: string | null;
+					creation_date: string;
+					finished: boolean;
+					id: string;
+					last_edited_date: string;
+					parent_list: string | null;
+					parent_task: string | null;
+				};
+				Insert: {
+					content?: string | null;
+					creation_date?: string;
+					finished?: boolean;
+					id?: string;
+					last_edited_date?: string;
+					parent_list?: string | null;
+					parent_task?: string | null;
+				};
+				Update: {
+					content?: string | null;
+					creation_date?: string;
+					finished?: boolean;
+					id?: string;
+					last_edited_date?: string;
+					parent_list?: string | null;
+					parent_task?: string | null;
+				};
+				Relationships: [
+					{
+						foreignKeyName: 'tasks_parent_list_fkey';
+						columns: ['parent_list'];
+						referencedRelation: 'lists';
+						referencedColumns: ['id'];
+					}
+				];
+			};
+		};
+		Views: {
+			[_ in never]: never;
+		};
+		Functions: {
+			[_ in never]: never;
+		};
+		Enums: {
+			[_ in never]: never;
+		};
+		CompositeTypes: {
+			[_ in never]: never;
+		};
+	};
 }
