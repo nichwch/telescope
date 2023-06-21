2a862c8885f9b552fe89fac1d7c2f26256ced8bf
nichwch <nichwch@gmail.com>
2023-06-21 00:55:48 -0400

    fix flushing strategic goal

--- src/routes/app/[listId]/+layout.svelte
+++ src/routes/app/[listId]/+layout.svelte
@@ -5,23 +5,21 @@
 
 	export let data;
 	const { supabase } = data;
-	$: strategic_goal_input = data.listContent?.[0].strategic_goal || '';
-	// $: strategic_goal_input = data.listContent?.[0].strategic_goal || '';
+	let strategic_goal_input = $page.data.listContent?.[0].strategic_goal || '';
+
 	let last_strategic_goal_input = strategic_goal_input;
 	let isFlushing = false;
 	const updateInterval = setInterval(async () => {
 		if (strategic_goal_input === last_strategic_goal_input) return;
 		if (isFlushing) return;
 		isFlushing = true;
-		console.log('flushing', strategic_goal_input);
 		supabase
 			.from('lists')
 			.update({
 				strategic_goal: strategic_goal_input
 			})
 			.eq('id', $page.params.listId)
-			.then((res) => {
-				console.log('flushed!');
+			.then(() => {
 				isFlushing = false;
 				last_strategic_goal_input = strategic_goal_input;
 			});
@@ -38,7 +36,10 @@
 			<div class="mb-2 border border-gray-500 bg-green-100 flex-grow w-80 flex flex-col">
 				<div class="px-4 py-1 border-b border-b-gray-500">strategic goal</div>
 				<textarea
-					bind:value={strategic_goal_input}
+					value={$page.data.listContent?.[0].strategic_goal}
+					on:input={(e) => {
+						strategic_goal_input = e?.target?.value || '';
+					}}
 					class="p-4 bg-green-100 flex-grow w-full block resize-none focus:outline-none focus:bg-green-200 transition-all"
 					placeholder="describe your large-level goals for this project..."
 				/>
