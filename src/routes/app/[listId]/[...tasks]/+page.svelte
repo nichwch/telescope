e7b112af44b5d16ad9090f73e7e75d221212393d
nichwch <nichwch@gmail.com>
2023-06-25 20:10:58 -0400

    bug fix

--- src/routes/app/[listId]/[...tasks]/+page.svelte
+++ src/routes/app/[listId]/[...tasks]/+page.svelte
@@ -4,26 +4,15 @@
 	import { nanoid } from 'nanoid';
 	import { fade, fly } from 'svelte/transition';
 	import { page } from '$app/stores';
-	import Todo from '../../../../components/Todo.svelte';
 	import type { TODO } from '../../../../utils/types';
 	import { onDestroy } from 'svelte';
+	import Todo from '../../../../components/Todo.svelte';
 
 	export let data;
 	const { supabase } = data;
 	const {
 		params: { listId, tasks }
 	} = $page;
-	$: console.log('tasks', tasks, $page.params);
-
-	let currentNestedList = data.listContent?.[0].tasks_blob as TODO[];
-	for (let nestedTask of tasks.split('/')) {
-		if (currentNestedList) {
-			const foundTask = currentNestedList?.find((task) => task.id === nestedTask);
-			if (foundTask) {
-				currentNestedList = foundTask.children || [];
-			}
-		}
-	}
 
 	// remove dnd attributes
 	const cleanData = (arr: any[]) => {
@@ -34,28 +23,54 @@
 		});
 	};
 
-	const updateList = async (items: TODO[], listId: string) => {
+	let items = (data.listContent?.[0].tasks_blob || []) as TODO[];
+
+	let focusedItems: TODO[] = cleanData(items);
+	for (let nestedTask of tasks.split('/')) {
+		const foundTask = focusedItems.find((task) => task.id === nestedTask);
+		if (foundTask) {
+			focusedItems = cleanData(foundTask.children || []);
+		}
+	}
+	const updateList = async (
+		items: TODO[],
+		focusedItems: TODO[],
+		pathToUpdate: string[],
+		listId: string
+	) => {
+		let newItems = [...items];
+		let listPtr = newItems;
+		for (let path of pathToUpdate) {
+			const foundItem = listPtr.find((item) => item.id === path);
+			if (foundItem?.children) {
+				listPtr = foundItem.children;
+			} else if (foundItem) {
+				foundItem.children = cleanData(focusedItems);
+				listPtr = foundItem.children;
+			}
+		}
+		listPtr = cleanData(focusedItems);
+		items = newItems;
 		return await supabase
 			.from('lists')
 			.update({
-				tasks_blob: items
+				tasks_blob: newItems
 			})
 			.eq('id', listId);
 	};
-
-	let items = cleanData((data.listContent?.[0].tasks_blob as TODO[]) || []);
-	let lastFlushedItems: string = JSON.stringify(cleanData([...items]));
+	let lastFlushedItems: string = JSON.stringify(cleanData([...focusedItems]));
 	let isFlushing = false;
 
 	const updateInterval = setInterval(async () => {
-		const currentString = JSON.stringify(cleanData(items));
+		const currentString = JSON.stringify(cleanData(focusedItems));
 		if (lastFlushedItems === currentString) return;
 		if (isFlushing) return;
+		console.log(lastFlushedItems);
 		isFlushing = true;
-		updateList(items, listId)
+		updateList(items, focusedItems, tasks.split('/'), listId)
 			.then(() => {
 				isFlushing = false;
-				lastFlushedItems = JSON.stringify(cleanData([...items]));
+				lastFlushedItems = JSON.stringify(cleanData([...focusedItems]));
 			})
 			.catch((e) => {
 				console.error('error while flushing', e);
@@ -68,8 +83,8 @@
 
 	const flipDurationMs = 300;
 	const createTODO = () => {
-		items = [
-			...items,
+		focusedItems = [
+			...focusedItems,
 			{
 				id: nanoid(),
 				name: 'new todo'
@@ -77,10 +92,10 @@
 		];
 	};
 	function handleDndConsider(e: any) {
-		items = e.detail.items;
+		focusedItems = e.detail.items;
 	}
 	function handleDndFinalize(e: any) {
-		items = e.detail.items;
+		focusedItems = e.detail.items;
 	}
 
 	let focusedElement: string | undefined = undefined;
@@ -111,12 +126,12 @@
 		</div>
 
 		<section
-			use:dndzone={{ items, flipDurationMs, transformDraggedElement }}
+			use:dndzone={{ items: focusedItems, flipDurationMs, transformDraggedElement }}
 			on:consider={handleDndConsider}
 			on:finalize={handleDndFinalize}
 			class="overflow-y-auto flex-grow"
 		>
-			{#each items as item (item.id)}
+			{#each focusedItems as item (item.id)}
 				<div animate:flip={{ duration: flipDurationMs }} in:fly>
 					<Todo {item} />
 				</div>
