77f9d46e10de7406211fc839df47881f5d9855aa
nichwch <nichwch@gmail.com>
2023-06-28 18:59:00 -0400

    type for changeHandler

--- src/routes/app/[listId]/+layout.svelte
+++ src/routes/app/[listId]/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { fade } from 'svelte/transition';
 	import { page } from '$app/stores';
-	import { onDestroy } from 'svelte';
+	import { onDestroy, type FormEventHandler } from 'svelte';
 
 	export let data;
 	const { supabase } = data;
@@ -28,6 +28,10 @@
 	onDestroy(() => {
 		clearInterval(updateInterval);
 	});
+
+	const changeHandler: FormEventHandler<HTMLTextAreaElement> = (e) => {
+		strategic_goal_input = (e?.target as HTMLTextAreaElement)?.value || '';
+	};
 </script>
 
 {#key $page.params.listId}
@@ -37,9 +41,7 @@
 				<div class="px-4 py-1 border-b border-b-gray-500">strategic goal</div>
 				<textarea
 					value={$page.data.listContent?.[0].strategic_goal}
-					on:input={(e) => {
-						strategic_goal_input = e?.target?.value || '';
-					}}
+					on:input={changeHandler}
 					class="p-4 bg-green-100 flex-grow w-full block resize-none focus:outline-none focus:bg-green-200 transition-all"
 					placeholder="describe your large-level goals for this project..."
 				/>
