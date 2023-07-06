b856347c18eca7cddf65d1a649b516a6cc3c1f76
nichwch <nichwch@gmail.com>
2023-07-06 17:22:48 -0400

    order lists

--- src/routes/app/+page.ts
+++ src/routes/app/+page.ts
@@ -6,7 +6,10 @@ export const load = async ({ parent }) => {
 	if (!session?.user) {
 		throw error(401, 'Unauthorized');
 	}
-	const { data: lists } = await supabase.from('lists').select('*');
+	const { data: lists } = await supabase
+		.from('lists')
+		.select('*')
+		.order('created_date', { ascending: false });
 	return {
 		user: session?.user,
 		lists
