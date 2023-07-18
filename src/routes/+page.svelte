3eb63408861230db8d485672868360b20c43e99a
nichwch <nichwch@gmail.com>
2023-07-18 00:19:24 -0400

    telescope emoji

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="max-w-2xl mx-auto py-5 md:py-20">
-	<h1 class="text-2xl">telescope</h1>
+	<h1 class="text-2xl">telescope 🔭</h1>
 	<h2>a todo app for focused work</h2>
 	<div class="mt-3">
 		<a class="p-3 bg-slate-100 rounded-xl hover:bg-slate-200" href="/signin">open app</a>
