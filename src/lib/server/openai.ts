412c3218c8b5f6cf13ca5a186fd92e5a980690ca
nichwch <nichwch@gmail.com>
2023-07-20 17:35:45 -0400

    max token for response

--- src/lib/server/openai.ts
+++ src/lib/server/openai.ts
@@ -1,4 +1,8 @@
 import { OpenAI } from 'langchain/llms/openai';
 import { OPENAI_API_KEY } from '$env/static/private';
 
-export const model = new OpenAI({ openAIApiKey: OPENAI_API_KEY, temperature: 0.5 });
+export const model = new OpenAI({
+	openAIApiKey: OPENAI_API_KEY,
+	temperature: 0.5,
+	maxTokens: 500
+});
