ff09305528d53b283481dce39fc89439da3779d2
nichwch <nichwch@gmail.com>
2023-07-21 17:56:11 -0400

    weirdly, putting separate with commas after the custom instructions fixes the model outputting numbered output

--- src/lib/server/chains/simple_task_suggestion.ts
+++ src/lib/server/chains/simple_task_suggestion.ts
@@ -46,7 +46,7 @@ ${finished_subtasks_str}`
 Help them break down this task into more subtasks.${
 		task_prompt
 			? `
-Here are more instructions on how to break down this task: ${task_prompt}`
+Here are more instructions on how to break down this task: ${task_prompt} Separate with commas.`
 			: ''
 	}
 Do not repeat any subtasks that are already listed. Aim to provide around 7-10 items. ${formatInstructions}
