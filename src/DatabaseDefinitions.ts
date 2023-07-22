8538e3aa0518b065d66a99933ce646c590e75a9f
nichwch <nichwch@gmail.com>
2023-07-22 17:30:04 -0400

    new schema

--- src/DatabaseDefinitions.ts
+++ src/DatabaseDefinitions.ts
@@ -1,50 +1,113 @@
-export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
+Need to install the following packages:
+  supabase@1.77.9
+Ok to proceed? (y) export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
 
 export interface Database {
-	public: {
-		Tables: {
-			lists: {
-				Row: {
-					created_date: string | null;
-					id: string;
-					last_edited_date: string | null;
-					name: string | null;
-					owner: string;
-					strategic_goal: string | null;
-					tasks_blob: Json;
-				};
-				Insert: {
-					created_date?: string | null;
-					id?: string;
-					last_edited_date?: string | null;
-					name?: string | null;
-					owner: string;
-					strategic_goal?: string | null;
-					tasks_blob?: Json;
-				};
-				Update: {
-					created_date?: string | null;
-					id?: string;
-					last_edited_date?: string | null;
-					name?: string | null;
-					owner?: string;
-					strategic_goal?: string | null;
-					tasks_blob?: Json;
-				};
-				Relationships: [];
-			};
-		};
-		Views: {
-			[_ in never]: never;
-		};
-		Functions: {
-			[_ in never]: never;
-		};
-		Enums: {
-			[_ in never]: never;
-		};
-		CompositeTypes: {
-			[_ in never]: never;
-		};
-	};
+  public: {
+    Tables: {
+      lists: {
+        Row: {
+          created_date: string | null
+          id: string
+          last_edited_date: string | null
+          name: string | null
+          owner: string
+          strategic_goal: string | null
+          tasks_blob: Json
+        }
+        Insert: {
+          created_date?: string | null
+          id?: string
+          last_edited_date?: string | null
+          name?: string | null
+          owner: string
+          strategic_goal?: string | null
+          tasks_blob?: Json
+        }
+        Update: {
+          created_date?: string | null
+          id?: string
+          last_edited_date?: string | null
+          name?: string | null
+          owner?: string
+          strategic_goal?: string | null
+          tasks_blob?: Json
+        }
+        Relationships: []
+      }
+      tasks: {
+        Row: {
+          ai_generatedd: boolean
+          child_index: number
+          created_at: string
+          description: string
+          done: boolean
+          id: string
+          list_parent: string | null
+          name: string
+          owner: string
+          queued_done: boolean | null
+          task_parent: string | null
+        }
+        Insert: {
+          ai_generatedd?: boolean
+          child_index: number
+          created_at?: string
+          description?: string
+          done?: boolean
+          id?: string
+          list_parent?: string | null
+          name?: string
+          owner: string
+          queued_done?: boolean | null
+          task_parent?: string | null
+        }
+        Update: {
+          ai_generatedd?: boolean
+          child_index?: number
+          created_at?: string
+          description?: string
+          done?: boolean
+          id?: string
+          list_parent?: string | null
+          name?: string
+          owner?: string
+          queued_done?: boolean | null
+          task_parent?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "tasks_list_parent_fkey"
+            columns: ["list_parent"]
+            referencedRelation: "lists"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "tasks_task_parent_fkey"
+            columns: ["task_parent"]
+            referencedRelation: "tasks"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
 }
