46af9326196900e43592ab03c1e89256df8a2663
nichwch <nichwch@gmail.com>
2023-07-28 17:33:30 -0400

    persist focused task

--- src/routes/app/[listId]/[...tasks]/FocusedTaskDisplay.svelte
+++ src/routes/app/[listId]/[...tasks]/FocusedTaskDisplay.svelte
@@ -2,14 +2,36 @@
 	import { fly } from 'svelte/transition';
 	import { page } from '$app/stores';
 	import type { IntermediateTask } from '$lib/types';
+	import { onDestroy } from 'svelte';
 
 	export let focusedTask: IntermediateTask;
 	const {
-		params: { listId }
+		params: { listId },
+		data: { supabase }
 	} = $page;
+
+	let description = focusedTask.description;
+	let lastFlushedDescription = description;
+	let name = focusedTask.name;
+	let lastFlushedName = name;
+	let flushing = false;
 
 	$: history = $page.params.tasks;
 	$: segments = history?.split('/');
+
+	const updateFunction = async () => {
+		if (lastFlushedDescription === description && lastFlushedName === name && flushing) return;
+		flushing = true;
+		await supabase.from('tasks').update({ description, name }).match({ id: focusedTask.id });
+		flushing = false;
+		lastFlushedDescription = description;
+		lastFlushedName = name;
+	};
+	const updateInterval = setInterval(updateFunction, 300);
+
+	onDestroy(() => {
+		clearInterval(updateInterval);
+	});
 </script>
 
 <div class="border border-green-700 bg-green-50 text-green-800 flex flex-col" in:fly>
@@ -18,19 +40,15 @@
 			contenteditable
 			role="textbox"
 			class="block name_textarea resize-none break-word overflow-x-hidden flex-grow focus:outline-none cursor-text"
-			bind:innerText={focusedTask.name}
-		>
-			<!-- {focusedTask.name} -->
-		</div>
+			bind:innerText={name}
+		/>
 		<div
 			contenteditable
 			role="textbox"
 			class="block description_textarea resize-none
 	break-word overflow-x-hidden flex-grow focus:outline-none cursor-text"
-			bind:innerText={focusedTask.description}
-		>
-			<!-- {focusedTask.description} -->
-		</div>
+			bind:innerText={description}
+		/>
 	</div>
 	<div class="flex border-t border-t-green-700">
 		<a
