5c7ef675cbdd1f6abb011a01330f01994bdc0471
nichwch <nichwch@gmail.com>
2023-08-08 19:59:29 -0400

    copy button for code

--- src/routes/app/[listId]/[...tasks]/chat/CodeRenderer.svelte
+++ src/routes/app/[listId]/[...tasks]/chat/CodeRenderer.svelte
@@ -3,8 +3,14 @@
 	export let node: Code;
 	import hljs from 'highlight.js';
 	let highlightedCode = node.value;
+	let copied = false;
 	if (node.lang && hljs.getLanguage(node.lang)) {
 		highlightedCode = hljs.highlight(node.value, { language: node.lang! }).value;
+	}
+	$: if (copied) {
+		setTimeout(() => {
+			copied = false;
+		}, 1000);
 	}
 </script>
 
@@ -14,9 +20,17 @@
 		<button
 			class="ml-auto"
 			on:click={() => {
-				navigator.clipboard.writeText(node.value);
-			}}>copy code</button
+				navigator.clipboard.writeText(node.value).then(() => {
+					copied = true;
+				});
+			}}
 		>
+			{#if copied}
+				copied!
+			{:else}
+				copy code
+			{/if}
+		</button>
 	</div>
 	<pre class="p-4">
 <code class="block text-left">{@html highlightedCode}</code>
