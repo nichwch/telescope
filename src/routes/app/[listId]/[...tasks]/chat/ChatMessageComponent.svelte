838e0fe9ef6f246e8530dc97697ef62daef6006a
nichwch <nichwch@gmail.com>
2023-08-08 18:50:42 -0400

    parse markdown

--- src/routes/app/[listId]/[...tasks]/chat/ChatMessageComponent.svelte
+++ src/routes/app/[listId]/[...tasks]/chat/ChatMessageComponent.svelte
@@ -1,27 +1,15 @@
 <script lang="ts">
 	import type { Message } from 'ai';
-	import MarkdownIt from 'markdown-it';
-	import hljs from 'highlight.js';
 	import 'highlight.js/styles/github.css';
+	import MarkdownRenderer from './MarkdownTokenRenderer.svelte';
+	import { unified } from 'unified';
+	import remarkParse from 'remark-parse';
 	export let message: Message;
+	const parser = unified().use(remarkParse);
 
-	const md = new MarkdownIt({
-		highlight: function (str, lang) {
-			if (lang && hljs.getLanguage(lang)) {
-				console.log('lang', lang);
-				try {
-					return hljs.highlight(str, { language: lang }).value;
-				} catch (error) {
-					console.error({ error });
-				}
-			}
+	const tree = parser.parse(message.content);
 
-			return ''; // use external default escaping
-		}
-	});
-
-	$: htmlContent = md.render(message.content);
-	$: console.log('tokens', md.parse(message.content, {}));
+	console.log(tree);
 </script>
 
 {#if message.role !== 'system'}
@@ -30,7 +18,6 @@
 			<span class="font-bold">you: </span>
 		{:else if message.role == 'assistant'}<span class="font-semibold">AI: </span>
 		{/if}
-		{@html htmlContent}
 	</div>
 {/if}
 
---
+++ src/routes/app/[listId]/[...tasks]/chat/MarkdownTokenRenderer.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let tag = 'div';
+	export let content = '';
+	console.log({
+		tag,
+		content
+	});
+</script>
+
+<svelte:element this={tag}> {content}</svelte:element>
