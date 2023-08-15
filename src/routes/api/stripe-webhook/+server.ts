86a1b554bb846613a350fd93a9019bf6e500afea
nichwch <nichwch@gmail.com>
2023-08-15 19:45:59 -0400

    page with stripe upgrade buttons

--- src/routes/api/stripe-webhook/+server.ts
+++ src/routes/api/stripe-webhook/+server.ts
@@ -5,6 +5,7 @@ import Stripe from 'stripe';
 const stripe = new Stripe(STRIPE_API_KEY, {});
 export const POST: RequestHandler = async ({ request, locals }) => {
 	const json_body = await request.json();
+	let event;
 	if (WH_SEC) {
 		// Get the signature sent by Stripe
 		const signature = request.headers.get('stripe-signature');
@@ -15,7 +16,26 @@ export const POST: RequestHandler = async ({ request, locals }) => {
 			return error(400);
 		}
 	}
+	if (!event) return error(400);
 	console.log('webhook hit!');
 	console.dir(json_body, { depth: null });
+
+	// Handle the event
+	switch (event.type) {
+		case 'payment_intent.succeeded':
+			const paymentIntent = event.data.object;
+			console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+			// Then define and call a method to handle the successful payment intent.
+			// handlePaymentIntentSucceeded(paymentIntent);
+			break;
+		case 'payment_method.attached':
+			const paymentMethod = event.data.object;
+			// Then define and call a method to handle the successful attachment of a PaymentMethod.
+			// handlePaymentMethodAttached(paymentMethod);
+			break;
+		default:
+			// Unexpected event type
+			console.log(`Unhandled event type ${event.type}.`);
+	}
 	return new Response('Received', { status: 200 });
 };
---
+++ src/routes/app/upgrade/+page.svelte
@@ -0,0 +1,18 @@
+<div>
+	<h1>Plans:</h1>
+
+	<script async src="https://js.stripe.com/v3/buy-button.js">
+	</script>
+	<h2>telescope plus</h2>
+
+	<stripe-buy-button
+		buy-button-id="buy_btn_1NfWujIApHiFZdHmcB0BKvjd"
+		publishable-key="pk_live_x59sk9NbsFSdx9sqMI6uRyyb00SRK8BbrZ"
+	/>
+	<h2>telescope pro</h2>
+
+	<stripe-buy-button
+		buy-button-id="buy_btn_1NfWv2IApHiFZdHmKWM7NNIz"
+		publishable-key="pk_live_x59sk9NbsFSdx9sqMI6uRyyb00SRK8BbrZ"
+	/>
+</div>
