c0fae100d5578cfacba1643768a6cd953c3f3889
nichwch <nichwch@gmail.com>
2023-08-16 16:06:11 -0400

    add user id to checkout buttons

--- src/routes/app/upgrade/+page.svelte
+++ src/routes/app/upgrade/+page.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+	export let data;
+	const user_id = data.session?.user.id;
+</script>
+
 <div>
 	<h1>Plans:</h1>
 
@@ -6,12 +11,14 @@
 	<h2>telescope plus</h2>
 
 	<stripe-buy-button
+		client-reference-id={user_id}
 		buy-button-id="buy_btn_1NfWujIApHiFZdHmcB0BKvjd"
 		publishable-key="pk_live_x59sk9NbsFSdx9sqMI6uRyyb00SRK8BbrZ"
 	/>
 	<h2>telescope pro</h2>
 
 	<stripe-buy-button
+		client-reference-id={user_id}
 		buy-button-id="buy_btn_1NfWv2IApHiFZdHmKWM7NNIz"
 		publishable-key="pk_live_x59sk9NbsFSdx9sqMI6uRyyb00SRK8BbrZ"
 	/>
